7a3b9777c13c821ebe71966afadec71a68860c94
Mikol Faro <mikol.faro@gmail.com>
2020-10-11 18:28:42 +0200

    Fix camera

--- src/routes/Camera.svelte
+++ src/routes/Camera.svelte
@@ -11,8 +11,6 @@
     <header>
         <h1>{pageName} - {atem.state._pin}</h1>
         <a href="#switcher" class="tab"><Feather icon="grid"/>Switcher</a>
-        <a href="#media" class="tab"><Feather icon="film"/>Media</a>
-        <a href="#macros" class="tab"><Feather icon="box"/>Macros</a>
         <span class="tab connection-status" class:connected={ws.readyState === 1}
               title="Connection status: green=connected, red=disconnected">
             {#if ws.readyState === 1}<Feather icon="zap"/>{:else}<Feather icon="alert-triangle"/>{/if}
